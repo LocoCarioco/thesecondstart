69c6d4794c89c462ae416a686b6b37fb759bb059
Lococarioco test <dsitqa@mail.ru>
2025-07-10 14:21:41 +0300

    TheFirstTest

---
+++ src/test/java/TheFirstTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TheFirstTest {
+
+    @Test
+    void testOne(){
+        open("https://www.saucedemo.com/");
+        $("#user-name").setValue("standard_user");
+        $("#password").setValue("secret_sauce");
+        $("#login-button").click();
+        $(".app_logo").shouldHave(text("Swag Labs"));
+    }
+}
